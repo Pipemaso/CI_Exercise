69795669c2fdf7ab2744e15687e1e073440420a1
Pipemaso <dafelmar@gmail.com>
2023-10-04 18:13:48 -0500

    Added the comment in Java

---
+++ CI-E1/src/ci/e1/CIE1.java
@@ -0,0 +1,10 @@
+
+package ci.e1;
+
+public class CIE1 {
+
+    public static void main(String[] args) {
+        System.out.println("A commit has been made");
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CI_Exercise
+Added a the comment "A commit has been made"
